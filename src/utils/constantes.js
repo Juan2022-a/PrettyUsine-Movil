f3b1e805b09d2a4e456d1f20cb23e8817011c402
Juan2022-a <20220671@ricaldone.edu.sv>
2024-07-28 14:13:40 -0600

    mi ip

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -3,7 +3,10 @@
 //Ip de la maquina locala r
 //utilizar la direccion IP del servidor y no localhost
 
-export const IP = 'http://10.10.3.8'
+//export const IP = 'http://192.168.0.23' ip JP
+
+
+export const IP = 'http://192.168.0.23'
 
 //192.168.1.9 alekc
 // Verifica si la sesión está iniciada y si el usuario tiene permisos adecuados
